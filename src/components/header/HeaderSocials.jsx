ab2bb10d519c6ac7770c43ba0b78bd4aa33af92b
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-18 22:23:21 +0600

    icons and css

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {RiFacebookBoxFill} from 'react-icons/ri'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://www.linkedin.com/in/smshakib/" target="_blank">BsLinkedin</a>
+        <a href="https://github.com/doubleO-Seven" target="_blank">FaGithub</a>
+        <a href="https://www.linkedin.com/in/smshakib/" target="_blank">RiFacebookBoxFill</a>
+    </div>
+  )
+}
+
+export default HeaderSocials
