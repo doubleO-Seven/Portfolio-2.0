4259ef2c344fe7fef93439c865a251a0d17e073a
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-18 20:58:43 +0600

    update header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>S. M. Shakib</h1>
+        <h5 className="text-lite">Front-End Developer</h5>
+      </div>
+    </header>
   )
 }
 
