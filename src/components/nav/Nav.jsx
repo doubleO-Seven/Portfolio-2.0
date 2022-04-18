1293dfe0b03fb60e599aa34e2d4550a6e28cb25e
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-19 00:16:07 +0600

    navbar update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {TiHomeOutline} from 'react-icons/ti'
+import {RiUserLine} from 'react-icons/ri'
+import {BiBookHeart} from 'react-icons/bi'
+import{RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className={activeNav === '#' ? 'active' : ''}><TiHomeOutline/></a>
+      <a href="#about"><RiUserLine/></a>
+      <a href="#experience"><BiBookHeart/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
