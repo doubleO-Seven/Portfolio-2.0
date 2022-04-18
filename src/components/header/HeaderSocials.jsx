727460d899b77d817b770291c03d73c309ab8e9d
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-18 23:00:26 +0600

    header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {RiFacebookBoxFill} from 'react-icons/ri'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/smshakib/" target="_blank">BsLinkedin</a>
-        <a href="https://github.com/doubleO-Seven" target="_blank">FaGithub</a>
-        <a href="https://www.linkedin.com/in/smshakib/" target="_blank">RiFacebookBoxFill</a>
+        <a href="https://www.linkedin.com/in/smshakib/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/doubleO-Seven" target="_blank"><FaGithub/></a>
+        <a href="https://www.facebook.com/smshakib007" target="_blank"><RiFacebookBoxFill/></a>
     </div>
   )
 }
