adbdd75e8ce521f9ac0b97217c6a925061f4dfd9
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-18 21:39:44 +0600

    update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
@@ -8,6 +10,11 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>S. M. Shakib</h1>
         <h5 className="text-lite">Front-End Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
       </div>
     </header>
   )
