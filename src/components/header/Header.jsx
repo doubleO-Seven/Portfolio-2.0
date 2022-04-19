8ed48b94c214482a87d0bab4c7322c400624e6f9
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-19 14:27:50 +0600

    about done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>S. M. Shakib</h1>
+        <h1>S. M. Shakib Hossain</h1>
         <h5 className="text-lite">Front-End Developer</h5>
         <CTA/>
         <HeaderSocials/>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,11 +11,11 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" className={activeNav === '#' ? 'active' : ''}><TiHomeOutline/></a>
-      <a href="#about" onClick={ () => setActiveNav('#about')} className = {activeNav === '#' ? 'active' : '' }><RiUserLine/></a>
-      <a href="#experience"><BiBookHeart/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><BiMessageSquareDetail/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ' '}><TiHomeOutline/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className = {activeNav === '#about' ? 'active' : ' ' }><RiUserLine/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className = {activeNav === '#experience' ? 'active' : ' ' }><BiBookHeart/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className = {activeNav === '#services' ? 'active' : ' ' }><RiServiceLine/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className = {activeNav === '#contact' ? 'active' : ' ' }><BiMessageSquareDetail/></a>
     </nav>
   )
 }
