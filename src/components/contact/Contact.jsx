87587987d0473faf37d48221867b6573c1cd53d6
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-20 15:15:46 +0600

    updated mailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,19 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef ();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1lys4eo', 'template_6wx67b5', form.current, 'TpbfiBsiCa5eDezZx')
+      
+    e.target.reset()
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -32,7 +43,7 @@ const Contact = () => {
           </article>
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit = {sendEmail}>
           <input type="text" name ='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your email' required />
           <textarea name="message" rows="7" placeholder='Your message' required></textarea>
