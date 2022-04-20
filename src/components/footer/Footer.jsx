578ed34cddf9efd29bb23b0744dc6a4cb49d2cb9
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-20 15:33:56 +0600

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,23 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>S. M. Shakib</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__copyright">
+        <small>All rights reserved by S. M. Shakib</small>
+      </div>
+    </footer>
   )
 }
 
